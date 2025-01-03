ee486ddabb69e8ff1915c2b654377be95dd4680e
trangqtt <quach.trang@outlook.com>
2025-01-03 22:46:38 +0700

    ⚡️ 003

--- 003.ts
+++ 003.ts
@@ -1,3 +1,8 @@
 function searchInsert(nums: number[], target: number): number {
-    return nums.filter((num) => num < target).length;
-};
+  // return nums.filter((num) => num < target).length;
+  let i = 0;
+  while (nums[i] < target) {
+    i++;
+  }
+  return i;
+}
